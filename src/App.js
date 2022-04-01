eb94b5dc5982a4f74ffb02f8f000377c286f4c7b
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-01 13:46:05 +0400

    finished with the checkbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Button } from "./ui/button";
+import {Checkbox} from './ui/checkbox/checkbox'
 
 function App() {
   return (
     <div className="App">
       <Button type='button'className='btn'><span>BUTTON</span></Button>
+      <Checkbox></Checkbox>
     </div>
   );
 }
