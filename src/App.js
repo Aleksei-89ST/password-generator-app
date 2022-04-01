0c636bc6b83c8698fcae44d107d8b124da7c1691
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-01 15:26:30 +0400

    ready to select

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from "react";
-import { Button } from "./ui/button";
-import { Checkbox } from "./ui/checkbox";
-import { Input } from "./ui/input";
+import { Button } from "./ui/button/button";
+import { Checkbox } from "./ui/checkbox/checkbox";
+import { Input } from "./ui/input/input";
+import {Select} from './ui/select/select';
 import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <Button type="button" className="btn">
+      <Button type="button">
         <span>BUTTON</span>
       </Button>
       <Checkbox></Checkbox>
       <Input></Input>
+      <Select></Select>
     </div>
   );
 }
--- src/ui/button/button.js
+++ src/ui/button/button.js
@@ -1,11 +1,17 @@
 import React from "react";
-import styles from './button.module.css';
-import cx from 'clsx'
+import styles from "./button.module.css";
+import cx from "clsx";
 
-function Button({type,className,onClick,children}) {
+function Button({ type, className, onClick, children }) {
   return (
     <div>
-      <button type={type} className={cx (styles['button'],className)} onClick={onClick}>{children}</button>
+      <button
+        type={type}
+        className={cx(styles["button"], className)}
+        onClick={onClick}
+      >
+        {children}
+      </button>
     </div>
   );
 }
