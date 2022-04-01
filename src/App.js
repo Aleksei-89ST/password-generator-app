e92b1abb8891ae465a35f6057e6f94095002db40
Aleksei-89ST <leha.leonov.52@bk.ru>
2022-04-01 14:31:05 +0400

    finished with input

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import "./App.css";
+import React from "react";
 import { Button } from "./ui/button";
-import {Checkbox} from './ui/checkbox/checkbox'
+import { Checkbox } from "./ui/checkbox";
+import { Input } from "./ui/input";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Button type='button'className='btn'><span>BUTTON</span></Button>
+      <Button type="button" className="btn">
+        <span>BUTTON</span>
+      </Button>
       <Checkbox></Checkbox>
+      <Input></Input>
     </div>
   );
 }
